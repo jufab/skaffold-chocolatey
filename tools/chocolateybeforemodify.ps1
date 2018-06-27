748e27e79dc3195ab69d4e50c8fe0a2680acee4b
Julien FABRE <julien.fabre@pole-emploi.fr>
2018-06-27 18:06:44 +0200

    init commit with v0.6.0

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,20 @@
+﻿write-host "Attempting to stop any running skaffold processes, to allow upgrade"
+try
+{
+  $running = get-process skaffold
+}
+catch
+{
+  write-host "No running skaffold processes to stop"
+  $running = @()
+}
+ 
+if ($running)
+{
+  write-host "Stopping skaffold processes"
+  foreach($p in $running)
+  {
+    stop-process $p
+    write-verbose "Stopped $($p.ProcessName) $($p.Id)"
+  }
+}
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'skaffold'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$version = $env:ChocolateyPackageVersion
+$url = "https://github.com/GoogleContainerTools/skaffold/releases/download/v$version/skaffold-windows-amd64.exe"
+$urlSHA = "https://github.com/GoogleContainerTools/skaffold/releases/download/v$version/skaffold-windows-amd64.exe.sha256"
+$fileSHA = "$toolsPath\skaffold.sha256"
+Get-WebFile -Url $urlSHA -FileName $fileSHA
+$checksumContent = Get-Content -Path $fileSHA 
+$checksum,$filename = $checksumContent.split(' ')
+$checksumtype = "sha256"
+$destination = "$toolsPath\skaffold-windows-amd64.exe"
+
+
+if (Test-Path "$toolsPath\skaffold.exe") {
+  Remove-Item "$toolsPath\skaffold.exe"
+  Remove-Item "$toolsPath\*.sha256"
+  Uninstall-BinFile -Name "skaffold" -Path "$toolsPath"
+}
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url
+  checksum       = $checksum
+  checksumtype   = $checksumtype
+}
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 $checksumtype
+
+Rename-Item -Path "$toolsPath\skaffold-windows-amd64.exe" -NewName "skaffold.exe"
+Install-BinFile -Name "skaffold" -Path "$toolsPath"
