17c2a3750f3c3dabb11e77dd62e82ec6e36d90fa
Julien FABRE <julien.fabre.33@gmail.com>
2023-05-29 06:40:56 +0000

    1.39.13

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$checksum = "ee20cfdca353afd2a149920dc35b8528b6bfa9943f12622978e34d404b3b5945"
+﻿$checksum = "29a8e1590d1199a80b44aee51238906054b50145ab93eb8d450ff0faa9d4e719"
 $ErrorActionPreference = 'Stop';
 $packageName = 'skaffold'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
